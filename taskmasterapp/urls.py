e6bfa81f4233ab31f25c42e4567224e409dd6ca9
danielpoxa <daniel.poxa@hotmail.com>
2024-11-28 16:14:49 -0300

    subindo atualizações

--- taskmasterapp/urls.py
+++ taskmasterapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.login_view, name='login'),   
     path('list', views.tasklist_view, name='list'), 
+    path('add' , views.taskadd_view, name='add'),
 ]
--- taskmasterapp/views.py
+++ taskmasterapp/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect  # Importando o redirect corretamente
 
 def login_view(request):
-    return render(request, 'login/index.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('list')  # Usando o 'redirect' corretamente
+        else:
+            return render(request, 'login/index.html', {'error': 'Credenciais inválidas'})
+
+    return render(request, 'login/index.html')  # Aqui, corrigido o erro de indentação
 
 def tasklist_view(request): 
     return render(request, 'tasklist/index.html')
+
+def taskadd_view(request): 
+    return render(request, 'taskadd/index.html')
